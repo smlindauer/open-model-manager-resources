d6c868eaa592ecd600f6429a1ea1fbf5927ebf02
honxin <hongjie.xin@sas.com>
2020-02-11 16:25:26 -0500

    Prefer using 'dockerHost' field in all destination creation

--- addons/create_aws_destination.py
+++ addons/create_aws_destination.py
@@ -38,6 +38,12 @@ K8S_NAME = "K8s Name"
 # destination name
 dest_name = "MY_AWS_DEST_NAME"
 
+# AWS region
+AWS_REGION = "us-east-1"
+
+# the docker daemon is running on tcp socket
+DOCKER_HOST_URL = "tcp://yy.yy.yy.yy:2375"
+
 if K8S_NAME == "K8s Name":
     print("Please replace the values in the script with real ones before executing the script!")
     exit(1)
@@ -57,17 +63,20 @@ destination_aws_headers = {
 # create new destination, expecting 201
 print("Creating " + dest_name + " destination...")
 
+props = [{"name": "domainId", "value": DOMAIN_NAME}]
+props.append({"name": "region", "value": AWS_REGION})
+props.append({"name": "kubernetesCluster", "value": K8S_NAME})
+
+if DOCKER_HOST_URL != "":
+    props.append({"name": "dockerHost", "value": DOCKER_HOST_URL})
+
 destination_attrs = {
     "name":dest_name,
     "destinationType":"aws",
-     "properties": [{"name": "domainId",                
-                 "value": DOMAIN_NAME},
-                {"name": "region",                 
-                 "value": "us-east-1"},
-                {"name": "kubernetesCluster",                 
-                 "value": K8S_NAME}
-                   ]
+    "properties": props
 }
+
+#print(destination_attrs)
 
 destination = requests.post(destination_url, 
                        data=json.dumps(destination_attrs), headers=destination_aws_headers)
