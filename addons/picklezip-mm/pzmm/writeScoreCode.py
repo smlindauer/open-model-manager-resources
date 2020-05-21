90ce5ea8991347686ce9a2cb290d68774f4c00c8
Kristen Aponte <Kristen.Aponte@sas.com>
2020-05-21 19:23:14 -0400

    First round of text edits for EDMMMX-3465.

--- addons/picklezip-mm/pzmm/writeScoreCode.py
+++ addons/picklezip-mm/pzmm/writeScoreCode.py
@@ -1,16 +1,5 @@
-# Copyright SAS Institute
-#
-#  Licensed under the Apache License, Version 2.0 (the License);
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 import os
 
@@ -25,29 +14,29 @@ class ScoreCode():
                        pyPath=os.getcwd(), threshPrediction=None,
                        otherVariable=False):
         '''
-        Write python score code for model manager based on training data used
-        to generate the model pickle file. The python file will be included in 
-        the zip file to be imported into model manager.
+        Writes a Python score code file based on training data used
+        to generate the model pickle file. The python file is included in
+        the ZIP file to be imported into SAS Open Model Manager or SAS Model Manager.
         
-        The score code generated is designed to be a working template for any 
+        The score code that is generated is designed to be a working template for any
         Python model to be used in SAS Open Model Manager, but is not guaranteed 
         to work out of the box for scoring and publishing/validating.
         
-        Note that for categorical variables, the variable is split into all of
-        the possible categorical values of the variable and by default does NOT
-        include a catch-all [catVar]_Other variable to include any missing 
-        values or any values not found in the training dataset. If you have 
-        missing values or values not included in your training dataset, you
-        will need to set the OtherVariable option to true.
+        Note that for categorical variables, the variable is split into
+        the possible categorical values of the variable. Also, by default it does NOT
+        include a catch-all [catVar]_Other variable to store any missing
+        values or any values not found in the training data set. If you have
+        missing values or values not included in your training data set, you
+        must set the OtherVariable option to True.
         
         Parameters
         ---------------
         inputDF : dataframe
-            Dataframe containing training data, including only the predictor
+            A Data frame that contains training data, and includes only the predictor
             columns. This function currently only supports int(64), float(64),
             and str data types for scoring.
         targetDF : dataframe
-            Dataframe containing training data of the target variable.
+            A Data frame that contains training data of the target variable.
         modelPrefix : string
             Variable name for the model to be displayed in model manager 
             (i.e. hmeqClassTree + [Score.py || .pickle]).      
@@ -57,25 +46,25 @@ class ScoreCode():
             the format() command. 
             For example: '{}.predict_proba({})'.
         pickleName : string
-            Name of pickle file containing the model.
+            Name of the pickle file that contains the model.
         metrics : string list, optional
-            Scoring metrics for model manager. The default is a set of two
+            The scoring metrics for SAS Open Model Manager. The default is a set of two
             metrics: EM_EVENTPROBABILITY and EM_CLASSIFICATION.
         pyPath : string, optional
-            Local path of the score code file. The default is the current 
+            The local path of the score code file. The default is the current
             working directory.
         threshPrediction : float, optional
-            Prediction theshold for probability metrics. For classification,
+            The prediction threshold for probability metrics. For classification,
             below this threshold is a 0 and above is a 1.
         otherVariable : boolean, optional
-            Option for having a categorical other value for catching missing 
-            values or values not found in the training dataset. The default
-            is set to False.
+            The option for having a categorical other value for catching missing
+            values or values not found in the training data set. The default setting
+            is False.
     			
     		Yields
     		---------------
         '*Score.py'
-            Python score code file for model manager.
+            The Python score code file for SAS Open Model Manager.
         '''       
         
         inputVarList = list(inputDF.columns)
@@ -136,7 +125,7 @@ def score{modelPrefix}({', '.join(inputVarList)}):
                               columns = ['const', {', '.join(f"'{x}'" for x in newVarList)}],
                               dtype = float)''')
     
-        # insert the model into the provided predictMethod call
+        # Insert the model into the provided predictMethod call.
             predictMethod = predictMethod.format('_thisModelFit', 'inputArray')
             self.pyFile.write(f'''\n
     prediction = {predictMethod}''')
@@ -156,14 +145,14 @@ def score{modelPrefix}({', '.join(inputVarList)}):
             
     def splitStringColumn(self, inputSeries, otherVariable):
         '''
-        Split a column of string values into a number of new variables equal
+        Splits a column of string values into a number of new variables equal
         to the number of unique values in the original column (excluding None
-        values). Then write to file new statements that tokenize the newly
+        values). It then writes to a file the statements that tokenize the newly
         defined variables.
         
-        As an example: given a series named strCol with values ['A', 'B', 'C',
-        None, 'A', 'B', 'A', 'D'], designate the following new variables: 
-        strCol_A, strCol_B, strCol_D. Then write the following to file:
+        Here is an example: Given a series named strCol with values ['A', 'B', 'C',
+        None, 'A', 'B', 'A', 'D'], designates the following new variables:
+        strCol_A, strCol_B, strCol_D. It then writes the following to the file:
             strCol_A = np.where(val == 'A', 1.0, 0.0)
             strCol_B = np.where(val == 'B', 1.0, 0.0)
             strCol_D = np.where(val == 'D', 1.0, 0.0)
@@ -200,17 +189,17 @@ def score{modelPrefix}({', '.join(inputVarList)}):
     
     def checkIfBinary(self, inputSeries):
         '''
-        Check a pandas series to determine if the values are binary or nominal.
+        Checks a pandas series to determine if the values are binary or nominal.
         
         Parameters
         ---------------
         inputSeries : float or int series
-            Series with numeric values.
+            A series with numeric values.
         
         Returns
         ---------------
         isBinary : boolean
-            True if the series values are binary. False if the series values
+            The returned value is True if the series values are binary, and False if the series values
             are nominal.
         '''
         
