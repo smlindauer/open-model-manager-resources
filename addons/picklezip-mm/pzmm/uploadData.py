c8d5db30dd3d8c087516e807d09eb0cfb1f26397
Scott Lindauer <Scott.Lindauer@sas.com>
2020-06-01 15:25:21 -0400

    Adjustments for 3912 (allow non user input authentication).

--- addons/picklezip-mm/pzmm/uploadData.py
+++ addons/picklezip-mm/pzmm/uploadData.py
@@ -1,10 +1,19 @@
-# Copyright (c) 2020, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
+# Copyright SAS Institute
+#
+#  Licensed under the Apache License, Version 2.0 (the License);
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 # %%
 import os
-import platform
 
 import requests
 import getpass
@@ -12,69 +21,102 @@ import json
 
 # %%
 
+def getAccessToken(server, username=None, password=None):
+    '''
+    Retrieve access token from the host server for further API requests. There
+    are two options for retrieving an access token:
+        1. Only provide the host server in the arguments. The user will be 
+        prompted to input their username and password (the password will not
+        be displayed in the prompt, nor kept in memory). Five attempts are
+        allowed before the program exits.
+        2. Provide the username, password, and host server in the arguments.
+        After the authentication attempt, the password will be removed from
+        the memory.
+        
+    Parameters
+    ---------------
+    server : string
+        Name of the host server with a model manager installation. Includes 
+        the protocol specification (i.e. http://).
+    username : string
+        Username used for authentication to the server.
+    password : string
+        Password used for authentication to the server.
+    
+    Returns
+    ---------------
+    authToken : string
+        Access token from json file from the API post request. Used for 
+        further API requests to the host server.
+    '''
+    
+    authURI = '/SASLogon/oauth/token'
+    headersAuth = {
+            'accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic c2FzLmVjOg=='
+            }
+    authToken = ''
+    
+    if (username is not None) and (password is not None):
+        authBody = ('grant_type=password&username=' + username +
+                    '&password=' + password)
+        authReturn = requests.post(server + authURI,
+                                   data=authBody,
+                                   headers=headersAuth)
+        if authReturn.status_code == 200:
+            authToken = authReturn.json()['access_token']        
+            notAuthenticated = False
+        else:
+            notAuthenticated = True
+            print('The provided username and password combination failed to' + 
+                  ' authenticate. Please enter correct a username and' + 
+                  ' password combination.')
+    else:
+        notAuthenticated = True
+    
+    loginAttempts = 0
+    while (notAuthenticated and loginAttempts < 5): 
+        username = input('Enter username:')
+        password = getpass.getpass('Enter password for %s:' % username)
+        authBody = ('grant_type=password&username=' + username +
+                    '&password=' + password)
+        authReturn = requests.post(server + authURI,
+                                   data=authBody,
+                                   headers=headersAuth)
+        if authReturn.status_code == 200:
+            authToken = authReturn.json()['access_token']
+            notAuthenticated = False
+        else:
+            print('Please enter correct username and password.')
+            loginAttempts += 1
+    
+    password = ''
+    
+    return f'Bearer {authToken}'
+
+
 class ModelImport():
     
     def __init__(self, host):
         '''
-        Initializes the  ModelImport class with host location, user name, and password.
+        Initialize ModelImport class with host location and username/password.
         
         Parameters
         ---------------
         host : string
-            Name of the host server with a SAS Open Model Manager installation.
+            Name of the host server with a model manager installation.
         '''
         
         if host[:7] == 'http://':
             host = host[7:]
         self.host = host
         self.server = 'http://' + host
-        
-    def getAccessToken(self):
-        '''
-        Retrieves the access token from the host server for further API requests.
-        Requires user input for user name and password, but does not keep
-        password in memory.
-        
-        Returns
-        ---------------
-        authToken : string
-            Access token from JSON file from the API post request. Used for 
-            further API requests to the host server.
-        '''
-        
-        authURI = '/SASLogon/oauth/token'
-        headersAuth = {
-                'accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic c2FzLmVjOg=='
-                }
-        authToken = ''
-        username = ''
-        password = ''
-        notAuthenticated = True
-        
-        while notAuthenticated: 
-            username = input('Enter a user name:')
-            password = getpass.getpass('Enter the password for %s:' % username)
-            authBody = ('grant_type=password&username=' + username +
-                        '&password=' + password)
-            authReturn = requests.post(self.server + authURI,
-                                       data=authBody,
-                                       headers=headersAuth)
-            if authReturn.status_code == requests.codes.ok:
-                authToken = authReturn.json()['access_token']
-                notAuthenticated = False
-            else:
-                print('Please enter a valid user name and password.')
-        
-        password = ''
-        
-        return f'Bearer {authToken}'
-    
+            
     def findProjectID(self, projectName, authToken):
         '''
-        Given a project name, makes an API request to Model Repository API to find the 
-        project ID. If project ID is not found, creates a new project and returns
+        Given a project name, make an API request to model manager to find the 
+        project ID. If project ID is not found, create a new project and return
         its project ID.
         
         Parameters
@@ -93,22 +135,24 @@ class ModelImport():
         headers = {
                 'Origin': self.server,
                 'Authorization': authToken}
-        requestUrl = f'{self.server}/modelRepository/projects?limit=100000'
+        projectFilter = f'?filter=eq(name, \'{projectName}\')'
+        requestUrl = f'{self.server}/modelRepository/projects{projectFilter}'
         projectRequest = requests.get(requestUrl, headers=headers)
         
-        projectID = [x['id'] for x in projectRequest.json()['items'] if x['name']==projectName]
-        if not projectID:
-            print(f'A project with the name "{projectName}" could not be found.')
-            print(f'A new project with the name "{projectName}" is being created.')
+        try:
+            projectID = projectRequest.json()['items'][0]['id']
+        except IndexError:
+            print(f'No project named {projectName} could be found.')
+            print(f'Creating a new project named {projectName}.')
             projectID = self.createNewProject(projectName, authToken)
             return projectID
-        else:
-            return projectID
+            
+        return projectID
 
     def createNewProject(self, projectName, authToken):
         '''
-        Determines the Public repository folder ID, and then creates a new project 
-        in the common model repository to store the models.
+        Determine the public repository folder ID. Then create a new project 
+        on model manager to store models in.
         
         Parameters
         ---------------
@@ -141,30 +185,35 @@ class ModelImport():
         return newProject.json()['id']
     
     def importModel(self, modelPrefix, projectID=None,
-                    projectName=None, zPath=os.getcwd()):
+                    projectName=None, zPath=os.getcwd(),
+                    username=None, password=None):
         '''
-        Imports the zipped pickle file and corresponding Python and JSON files into
-        the common model repository using the 'import model' API request. 
+        Import zipped pickle file and corresponding python and json files into
+        model manager using the 'import model' API. 
         
         If the project ID is not known, provide the project name and an API
-        request searches for the project ID. If a project does not already exist, and you
-        do not provide either the project name or ID, the function  
-        creates a new project via an API request.
+        request will search for the project ID. If no project already exists, 
+        do not provide either the project name or ID and the function will 
+        create a new project via an API request.
         
         Parameters
         ---------------
         modelPrefix : string
-            Variable name for the model to be displayed in SAS Open Model Manager
+            Variable name for the model to be displayed in model manager 
             (i.e. hmeqClassTree + [Score.py || .pickle]).
         projectID : string, optional
-            Universally unique identifier string project identifier. The default value is None.
+            Universally unique identifier string project identifier. Default is None.
         projectName : string, optional
-            Project name for retrieving the project ID. The default value is None.
+            Project name for retrieving the project ID. Default is None.
         zPath : string, optional
-            Location for the archive ZIP file. The default value is the current
+            File location for the zipped archive. Default is the current
             working directory.
+        username : string
+            Username used for authentication to the server.
+        password : string
+            Password used for authentication to the server.            
         '''
-        authToken = self.getAccessToken()
+        authToken = getAccessToken(self.server, username, password)
         
         if projectID is None and projectName is not None:
             projectID = self.findProjectID(projectName, authToken)
@@ -196,15 +245,15 @@ class ModelImport():
             modelRequest.raise_for_status()
         except requests.exceptions.HTTPError:
             print('Model import failed: ' +
-                  f'A model with the name "{modelPrefix}" already exists.')
-            print('Please specify a unique file name for the ZIP file.')
-
-# The following code is obsolete and was deprecated after the November 2019 release.
+                  f'A model named {modelPrefix} already exists.')
+            print('Please adjust the zip file name appropriately.')
+# DEPRECIATED       
 #    def uploadPickle(pLocalPath, pRemotePath,
 #                     host, username, password=None, privateKey=None):
-#        TODO: Remove password from memory as in self.getAccessToken()
+#        #TODO: Remove password from memory as in self.getAccessToken() 
+#        #TODO: Obsoleted after 19w47 builds of model manager
 #        '''
-#        Uploads a local pickle file to a SAS Open Model Manager server via sftp. Set the
+#        Upload a local pickle file to a model manager server via sftp. Set the
 #        permission of the pickle file on the server to 777 to allow the score
 #        code to use the pickle file.
 #        
